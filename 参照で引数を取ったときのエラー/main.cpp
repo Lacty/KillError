85ba0bf4ff1f68a80b6b1b5281223dbb183c0370
U-PS14B22\vantan <akira206@gmail.com>
2015-04-11 21:57:02 +0900

    引数に参照を持たせた時のエラー

---
+++ 参照で引数を取ったときのエラー/main.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+
+class Test01 {
+private:
+
+  int x;
+
+public:
+
+  Test01() :
+  x(10) {}
+
+  int getX() {
+    return x;
+  }
+};
+
+class Test02 {
+private:
+
+  int x;
+
+public:
+
+  Test02() :
+  x(0) {}
+
+  void copy(int& x) {
+    this->x = x;
+  }
+};
+
+int main(){
+  Test01 test01;
+  Test02 test02;
+
+  test02.copy(test01.getX()); // err
+
+  // initial value of reference to non_const must be an lvalue
+  // const型をもたないリファレンスの初期値は左辺値でなければなりません
+  // リファレンスってのは引数のことかな？
+  // 代入や変更のできるlvalue(左辺値)ではない((どこかのサイトより
+
+  // 解決法
+  // void copy(const int& x)
+  // constを付けてあげよう。
+
+  return 0;
+}
